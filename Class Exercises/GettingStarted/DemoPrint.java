e6b1ef431ed00dd24a27efedd5ad419c21bc08a6
A-L-M <al3cmills@gmail.com>
2018-01-09 12:47:00 -0700

    class dirs as specd by proffesor

---
+++ Class Exercises/GettingStarted/DemoPrint.java
@@ -0,0 +1,19 @@
+/**************************************
+* Author: Alec Mills                  *
+* Demo: how to print                  *
+**************************************/
+public class DemoPrint {
+    public static void main (String[] args) {
+        System.out.println("Hello, dude");      
+        System.out.println("How r U ?\n");
+        
+        System.out.println("  @..@");
+        System.out.println(" (----)");
+        System.out.println("( >  < )");
+        System.out.println("^^ ~~ ^^\n");
+        
+        System.out.println("(\\(\\");
+        System.out.println("(-.-)");
+        System.out.println("O_(\")(\")");
+    }
+}
