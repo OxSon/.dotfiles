9cfe5183822f10b23a53c2206fdad05c39a0065d
A-L-M <al3cmills@gmail.com>
2018-01-18 10:52:21 -0700

    add A01

---
+++ Assignments/A01.java
@@ -0,0 +1,26 @@
+/***************************
+ * Author: Alec Mills
+ * Date: 01/17/2018
+ * Assignment: A01 - Print
+ ****************************/
+
+public class A01 {
+    public static void main (String[] args) {
+        System.out.println("What I hope to get out of this class:");
+        System.out.println("\tI'm excited to be in this class. I have"
+                + " done some independent study\non the subject,but"
+                + " never a formal class. I hope to learn best"
+                + " practices\nfor style, design, and implementation."
+                + " I also hope to get an understanding\n"
+                + "of OOP principles. Finally, I hope to meet"
+                + " passionate programmers!\n");
+
+        System.out.println("What I am willing to put into this class:");
+        System.out.println("\tI plan to spend a lot of time outside of class "
+                + "reading up on relevant\nmaterial. I do so already and "
+                + "I really enjoy it. I plan to use group\nassignments "
+                + "to practice my \"soft\" skills. I'm looking forward "
+                + "to the\nchallenges in this class: intellectual, social, "
+                + "creative, etc.");
+    }
+}
