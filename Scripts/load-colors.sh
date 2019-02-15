d7f6de985211eb08dfd6d443b74421487b512378
OxSon <al3cmills@gmail.com>
2019-02-14 19:34:13 -0700

    update  colors sscript

--- Scripts/load-colors.sh
+++ Scripts/load-colors.sh
@@ -3,19 +3,26 @@
 #if a file path was passed, move it to our permanent bg location
 
 if [ $# -ge 1 ]; then
-    cp -f $1 ~/.config/wall.png
-    #generate color scheme using file specified
-    wal -c
-    wal -tg -i ~/.config/wall.png --backend schemer2
-
+    FILE= $1
+    #move it to permanent wallpaper location
+    cp $FILE ~/.config/wall.png
 else
-    #generate color scheme using a random file
-    wal -c
-    wal -tg -i "$(find $HOME/Pictures/Bgs -iname '*.*' | sort -R | tail -n1)" --backend schemer2
+    FILE="$(find $HOME/Pictures/Bgs -iname '*.*' | sort -R | tail -n1)"
+    #move it to permanent wallpaper location
+    cp $FILE ~/.config/wall.png
 fi
+
+
+
+#generate color scheme using file specified
+wal -c
+wal -tg -i ~/.config/wall.png --backend schemer2
+
+#update lockscreen
+betterlockscreen -u ~/.config/wall.png
 
 #config qutebrowser
 python ~/Scripts/qutebrowser_reload.py &
 #config jetbrains IDE's
-~/Scripts/intellijPywal/intellijPywalGen.sh ~/.IntelliJIdea2018.2/config
-~/Scripts/intellijPywal/intellijPywalGen.sh ~/.Rider2018.2/config
+#~/Scripts/intellijPywal/intellijPywalGen.sh ~/.IntelliJIdea2018.2/config #not currently using this
+#~/Scripts/intellijPywal/intellijPywalGen.sh ~/.Rider2018.2/config
